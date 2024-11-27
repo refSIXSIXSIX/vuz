788d5bde9195c5e200e487111d79b318e204f2d7
refSIXSIXSIX <groxa8701@gmail.com>
2024-11-27 16:19:27 +0300

    Ready

--- main.py
+++ main.py
@@ -12,4 +12,30 @@ def main():
     else:
         print("Email некорректный.")
 
-main()   
+class TestEmailValidation(unittest.TestCase):
+    def test_valid_emails(self):
+        self.assertTrue(is_valid_email("lazzy@test.com"))
+        self.assertTrue(is_valid_email("lazzy.2wice@example.vog.uk"))
+        self.assertTrue(is_valid_email("123@ass.org"))
+
+    def test_invalid_emails(self):
+        self.assertFalse(is_valid_email("lazzy.com"))
+        self.assertFalse(is_valid_email("ulazzy@.com"))
+        self.assertFalse(is_valid_email("lazzy@other"))
+def run_tests():
+    import unittest
+    print("Запуск тестов...")
+    unittest.main()
+
+if __name__ == "__main__":
+    print("What you need?\n")
+    print("1 - Main")
+    print("2 - Test")
+    choice = input()
+
+    if choice == "1":
+        main()
+    elif choice == "2":
+        run_tests()
+    else:
+        print("Неверный выбор. Пожалуйста, выберите 1 или 2.")
